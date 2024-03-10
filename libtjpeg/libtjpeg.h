ceebd02c7494704168f9effb3d61150cefa12cd2
toot_hzf <toot@tootzoe.com>
2024-03-10 16:34:17 +0700

    Initial Commit

---
+++ libtjpeg/libtjpeg.h
@@ -0,0 +1,24 @@
+//
+//  libtjpeg.h
+//  libtjpeg
+//
+//  Created by thor on 10/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libtjpeg.
+FOUNDATION_EXPORT double libtjpegVersionNumber;
+
+//! Project version string for libtjpeg.
+FOUNDATION_EXPORT const unsigned char libtjpegVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libtjpeg/PublicHeader.h>
+
+
